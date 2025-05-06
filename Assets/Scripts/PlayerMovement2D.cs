66aa6bacb49b92db9bb766a508ba9df8376b8660
Carlos Emilio Guidi Cordero <emiliogc06@gmail.com>
2025-05-06 09:42:46 -0300

    stash 1 old bk

---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement2D : MonoBehaviour
+{
+    [SerializeField] private InputActionReference Move, Intereact, PointerPosition;
+    [SerializeField] private float moveSpeed = 7f;
+    private Vector2 pointerInput, movementInput;
+
+    private Rigidbody2D rb;
+
+    private float moveInputHorizontal = 0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = Move.action.ReadValue<Vector2>();
+        moveInputHorizontal = inputVector.x;
+    }
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInputHorizontal = 0f;
+    }
+    private void OnEnable()
+    {
+        Move.action.Enable();
+        Move.action.performed += OnMovePerformed;
+
+        Move.action.canceled += OnMoveCanceled;
+    }
+    private void OnDisable()
+    {
+        if (Move.action != null) 
+        {
+
+            Move.action.Disable();
+
+            Move.action.performed -= OnMovePerformed;
+            Move.action.canceled -= OnMoveCanceled;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(moveInputHorizontal * moveSpeed, rb.linearVelocity.y);
+
+        rb.linearVelocity = targetVelocity;
+    }
+}
